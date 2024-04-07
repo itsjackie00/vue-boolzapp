b93f0453b0e420bdb658584df4884a3b9d7f94f9
itsjackie00 <jackiedanso33@gmail.com>
2024-04-07 17:36:26 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { contacts } from './data.js';
+import Picker from './emoji-picker.js';
 
 const dt = luxon.DateTime;
 
@@ -11,12 +12,13 @@ createApp({
             activeContactId: 1,
             messageText: '',
             letters: '',
+            activeMsgIndex: null,
+            // showEmoji: false,
         }
     },
     methods: {
         setActiveContact(id) {
             this.activeContactId = id
-            this.activeMsgIndex = null ;
         },
         createMessage() {
             const newMessage = {
@@ -34,30 +36,30 @@ createApp({
                 }
                 this.activeContact.messages.push(newMessage)
             }, 1000);
-        },        
+        },
         filterConctacts() {
             let array = this.contacts.filter((el) => el.name.toLowerCase().includes(this.letters));
-            console.log(array); 
-            return(array);//this.contacts.find((el) => el.name.includes(this.letters));
+            //console.log(array);
+            return (array);//this.contacts.find((el) => el.name.includes(this.letters));
 
         },
         //create a function for on click toggle down menu and show menu
         toggleDropdown(index) {
-            this.activeMsgIndex = this.activeMsgIndex === index ? null : index
+            this.activeMsgIndex = this.activeMsgIndex === index ? null : index;
         },
         getContactsIndex(id) {
-          const index = this.contacts.findIndex((el) => el.id === id);
-          const msgLastIndex = this.contacts[index].messages.length - 1;
-          if(msgLastIndex >= 0){
-            return this.contacts[index].messages[msgLastIndex];
-          }else{
-            return 'Non ci sono messaggi';
-          }
-          return this.contacts[index].messages[msgLastIndex];  
+            const index = this.contacts.findIndex((el) => el.id === id);
+            const msgLastIndex = this.contacts[index].messages.length - 1;
+            if (msgLastIndex >= 0) {
+                return this.contacts[index].messages[msgLastIndex];
+            } else {
+                return 'Non ci sono messaggi';
+            }
+            //return this.contacts[index].messages[msgLastIndex];
         },
         getLastMessage(id) {
-            const index= this.contacts.findIndex((el) => el.id === id);
-            const msgLastIndex  = this.contacts[index].messages.length - 1;
+            const index = this.contacts.findIndex((el) => el.id === id);
+            const msgLastIndex = this.contacts[index].messages.length - 1;
             return this.contacts[index].messages[msgLastIndex].message;
         },
         getLastMsgDate(id) {
@@ -65,27 +67,38 @@ createApp({
             const msgLastIndex = this.contacts[index].messages.length - 1;
             return this.contacts[index].messages[msgLastIndex].date;
         },
-        deletMsg(i){
-            this.activeContact.messages.splice(i,1);
-            this.activeMsgIndex = null ;
+        deletMsg(i) {
+            this.activeContact.messages.splice(i, 1);
         },
-        activeMsgIndex(){
-            return this.activeMsgIndex
-        }
+        
+        // onSelectEmoji(emoji) {
+        //     console.log(emoji)
+        //     this.messageText += emoji.i;
+        //     /*
+        //       // result
+        //       { 
+        //           i: "😚", 
+        //           n: ["kissing face"], 
+        //           r: "1f61a", // with skin tone
+        //           t: "neutral", // skin tone
+        //           u: "1f61a" // without tone
+        //       }
+        //       */
+        // },
     },
     computed: {
         activeContact() {
             return this.contacts.find((el) => el.id ===
                 this.activeContactId);
         },
-        lastAcccess(){
+        lastAcccess() {
             const index = this.activeContact.messages.length - 1;
             return this.activeContact.messages[index].date
         },
-        
+
     },
     mounted() {
-        console.log(this.contacts);
-        console.log(this.letters);
+        //console.log(this.contacts);
+        //console.log(this.letters);
     }
 }).mount('#app') 
